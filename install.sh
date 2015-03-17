bf08f8f42e14283dddb491225a1e11cd7eacc2d3
Jefferson B Hudson IV <jehudson@beaglebone.(none)>
2015-03-17 13:52:11 +0000

    added .bashrc and updated the install script

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 touch ~/.vimrc
+touch ~/.bashrc
+touch ~/.dircolors
+
 rm ~/.vimrc
+rm ~/.bashrc
+rm ~/.dircolors
+
 ln -s .vim/.vimrc ~/.vimrc
+ln -s .vim/.bashrc ~/.bashrc
+ln -s .vim/.dircolors ~/.dircolors
 
